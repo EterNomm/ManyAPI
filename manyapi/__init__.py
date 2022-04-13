c1a77c113d16185e34c6fa319dfa22d078728fca
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-13 12:06:22 +0700

    Update __init__.py

--- manyapi/__init__.py
+++ manyapi/__init__.py
@@ -1,1 +1,7 @@
 from .discord import *
+
+__title__ = "Whitehat"
+__version__ = "1.0b1"
+__authors__ = "LyQuid and EterNomm"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2022-present EterNomm"
