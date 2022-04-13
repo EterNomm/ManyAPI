18479d7e71ebdab2bf9dc34869a439c2a7e9f910
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-13 14:02:31 +0700

    Added `send_message` function

--- manyapi/discord.py
+++ manyapi/discord.py
@@ -1,4 +1,5 @@
 from .DiscordAPI.webhook import webhook
+from .DiscordAPI.send_message import send_message
 
 class Discord:
 	class webhook:
@@ -53,3 +54,15 @@ class Discord:
 			
 		def post(self):
 			return self.wh.post()
+			
+			
+			
+	class send_message:
+		def __init__(self, token:str, channel_id:int, api_version:int=9):
+			self.sm = send_message(token, channel_id, api_version)
+		
+		def tag_user(self, user_id:int):
+			return self.sm.tag_user(user_id)
+		
+		def send(self, message:str):
+			return self.sm.send(message)
