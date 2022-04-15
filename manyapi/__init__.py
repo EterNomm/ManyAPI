fbbb3fe2f03ed27978a0f8f0fec3f4b63c1821f9
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-15 14:05:22 +0700

    Update __init__.py

--- manyapi/__init__.py
+++ manyapi/__init__.py
@@ -4,7 +4,7 @@ from .randomword import Randomword
 from .youtube import Youtube
 
 __title__ = "Whitehat"
-__version__ = "1.0b1"
+__version__ = "1.0"
 __authors__ = "LyQuid and EterNomm"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2022-present EterNomm"
