c9efa380754e27d1f61278fbc3b55c86c5c12ac4
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-17 14:30:34 +0700

    Added `googlesearch` and `pypistats`

--- manyapi/__init__.py
+++ manyapi/__init__.py
@@ -2,9 +2,11 @@ from .discord import Discord
 from .anonfiles import Anonfiles
 from .randomword import Randomword
 from .youtube import Youtube
+from .googlesearch import Googlesearch
+from .pypi import PypiStats
 
-__title__ = "Whitehat"
-__version__ = "1.0"
+__title__ = "ManyAPI"
+__version__ = "2.0"
 __authors__ = "LyQuid and EterNomm"
-__license__ = "Apache License 2.0"
+__license__ = "GNU General Public License 3.0"
 __copyright__ = "Copyright 2022-present EterNomm"
